9c4c25e0e2987b961c05a3f66cf59804b5d33d98
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2025-02-16 14:22:13 +0100

    feat: add average update time

--- generate_readme.rb
+++ generate_readme.rb
@@ -25,24 +25,26 @@ def last_commit_time(git_link)
 	@last_commit_times[git_link] = commit_time
 end
 
-def generate_table(hyperfine_output_path, information_path, update_times_path, stream = $stdout, extra_rows = [])
+def generate_table(hyperfine_output_path, hyperfine_update_path, update_times_path, information_path, stream = $stdout, extra_rows = [])
 	results = JSON.load_file(hyperfine_output_path)['results']
+	results_update = JSON.load_file(hyperfine_update_path)['results']
 	info = CSV.read(information_path, col_sep: "\t", headers: true).to_h { [_1['command'], _1] }
 	update_times = CSV.read(update_times_path).to_h
 
-	headers = ["Client","Language", "Mean (ms)", "Deviation (ms)", "Time to Update Cache", "Last Commit", *extra_rows.map(&:first)]
+	headers = ["Client","Language", "Time to show `tar` page (mean ± σ)", "Time to Generate Cache", "Time to check for cache update (mean ± σ)", "Last Commit", *extra_rows.map(&:first)]
 
 	rows = results.map do |result|
 		command = result['command'][%r( \./(.*) tar), 1]
+		update = results_update.find { _1['command'][command] }
 		info_row = info[command]
 		update_times[command][/(\d+)m(\d+)\.(\d+)s/]
 		update_time = "%02d:%02d.%d" % [$1.to_i, $2.to_i, $3.to_i]
 		[
 			"[#{command}](#{info_row['git link']})",
 			info_row['lang'],
-			(result['mean'] * 1_000).round(1),
-			(result['stddev'] * 1_000).round(1),
+			"#{(result['mean'] * 1_000).round(1)} ms ± #{(result['stddev'] * 1_000).round(1)}",
 			update_time,
+			"#{(update['mean'] * 1_000).round(1)} ms ± #{(update['stddev'] * 1_000).round(1)}",
 			last_commit_time(info_row['git link']),
 			*extra_rows.map { _2.call(command, result, info_row, update_time) },
 		]
@@ -59,13 +61,13 @@ def generate_table(hyperfine_output_path, information_path, update_times_path, s
 end
 
 io = StringIO.new
-generate_table(ARGV[0], 'information.tsv', ARGV[1], io, [
+generate_table(*ARGV[0, 3], 'information.tsv', io, [
 	["Comment", ->(_, _, info, _) { info['comment mac'] }]
 ])
 mac_table = io.string
 
 io = StringIO.new
-generate_table(ARGV[2], 'information.tsv', ARGV[3], io, [
+generate_table(*ARGV[3, 3], 'information.tsv', io, [
 	["Comment", ->(_, _, info, _) { info['comment ubuntu'] }]
 ])
 ubuntu_table = io.string
